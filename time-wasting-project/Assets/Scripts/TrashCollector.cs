381163b27e289776f00f4046a2e193f373bd2d02
Steppe Schoolkate <S.Schoolkate@student.han.nl>
2021-10-13 12:40:50 +0200

    did the thinng, boss

--- time-wasting-project/Assets/Scripts/TrashCollector.cs
+++ time-wasting-project/Assets/Scripts/TrashCollector.cs
@@ -2,27 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class TrashCollector : MonoBehaviour
 {
 
+    [SerializeField] private AudioClip receiveClip;
+    [SerializeField] private AudioClip loopClip;
+    
     private Transform transform;
     private LiftableObject liftableObject;
+
+    private AudioSource audioSource;
+    
+    
+    
     
     // Start is called before the first frame update
     void Start()
     {
         transform = GetComponent<Transform>();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     public void GiveShit(LiftableObject shit)
     {
-        
+        audioSource.clip = receiveClip;
+        audioSource.Play();
     }
     
     private void OnCollisionEnter(Collision other)
